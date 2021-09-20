8aa556c3bac38674fdaa83b3badc8b13ba750a2e
Anna Yan <annayan102@gmail.com>
2021-09-20 16:42:17 -0400

    Implemented _.contains and _.every of Part 2

--- src/underbar.js
+++ src/underbar.js
@@ -207,6 +207,13 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    return _.reduce(collection, function(memo, item) {
+      if (!memo) {
+        return false;
+      }
+      return Boolean(iterator(item));
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
